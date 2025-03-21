038409199c6b25cf5c5c6cc7d1374048f0819e63
ktmth <k1ngm1n@naver.com>
2025-03-21 21:34:16 +0900

    add

---
+++ Sort/Sort/sort.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+#define LENGTH 100
+
+void InsertionSort(int* A) {
+	for (int j = 1; j < LENGTH; j++) {
+		int key = A[j];
+		int i = j - 1;
+
+		while (i >= 0 && A[i] > key) {
+			A[i + 1] = A[i];
+			i--;
+		}
+		A[i + 1] = key;
+	}
+}
+
+void BubbleSort(int* A) {
+	for (int i = 0; i < LENGTH - 1; i++) {
+		for (int j = 0; j < LENGTH - i - 1; j++) {
+			if (A[j] > A[j + 1]) {
+				int key = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = key;
+			}
+		}
+	}
+}
+
+void MergeSort(int* A) {
+
+}
+
+void QuickSort(int* A) {
+
+}
+
+void RandomNum(int* A) {
+	srand(time(0));
+	for (int i = 0; i < LENGTH; i++) {
+		A[i] = rand() % 100 + 1;
+	}
+}
+
+void Output(int* A) {
+	for (int i = 0; i < LENGTH; i++) {
+		cout << A[i] << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+	int A[LENGTH];
+
+	RandomNum(A);
+	cout << "Random: \n";
+	Output(A);
+
+	int choice;
+	cout << "1: Insertion Sort\n";
+	cout << "2: Bubble Sort\n";
+	cout << "3: Merge Sort\n";
+	cout << "4: Quick Sort\n";
+	cin >> choice;
+
+	switch (choice) {
+	case '1':
+		InsertionSort(A);
+	case '2':
+		BubbleSort(A);
+	case '3':
+		MergeSort(A);
+	case '4':
+		QuickSort(A);
+	default:
+		cout << "wrong Num" << endl;
+		return 1;
+	}
+	Output(A);
+
+	return 0;
+}
