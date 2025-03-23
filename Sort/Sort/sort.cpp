b5c2e2630066d4c9301a06626594a311384e31bc
ktmth <k1ngm1n@naver.com>
2025-03-23 21:35:18 +0900

    add

--- Sort/Sort/sort.cpp
+++ Sort/Sort/sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <climits>
 using namespace std;
 
 #define LENGTH 100
@@ -30,12 +31,63 @@ void BubbleSort(int* A) {
 	}
 }
 
-void MergeSort(int* A) {
+void Merge(int* A, int p, int q, int r) {
+	int n1 = q - p + 1;
+	int n2 = r - q;
 
+	int* L = new int[n1 + 1];
+	int* R = new int[n2 + 1];
+
+	for (int i = 0; i < n1; i++)
+		L[i] = A[p + i];
+	for (int j = 0; j < n2; j++)
+		R[j] = A[q + j + 1];
+
+	L[n1] = INT_MAX;
+	R[n2] = INT_MAX;
+
+	int i = 0;
+	int j = 0;
+	for (int k = p; k <= r; k++) {
+		if (L[i] <= R[j]) {
+			A[k] = L[i];
+			i++;
+		}
+		else {
+			A[k] = R[j];
+			j++;
+		}
+	}
+
+	delete[] L;
+	delete[] R;
 }
-
-void QuickSort(int* A) {
-
+void MergeSort(int* A, int p, int r) {
+	if (p < r) {
+		int q = (p + r) / 2;
+		MergeSort(A, p, q);
+		MergeSort(A, q + 1, r);
+		Merge(A, p, q, r);
+	}
+}
+int Partition(int* A, int p, int r) {
+	int x = A[r];
+	int i = p - 1;
+	for (int j = p; j < r; j++) {
+		if (A[j] <= x) {
+			i++;
+			swap(A[i], A[j]);
+		}
+	}
+	swap(A[i + 1], A[r]);
+	return i + 1;
+}
+void QuickSort(int* A, int p, int r) {
+	if (p < r) {
+		int q = Partition(A, p, r);
+		QuickSort(A, p, q - 1);
+		QuickSort(A, q + 1, r);
+	}
 }
 
 void RandomNum(int* A) {
@@ -67,14 +119,18 @@ int main() {
 	cin >> choice;
 
 	switch (choice) {
-	case '1':
+	case 1:
 		InsertionSort(A);
-	case '2':
+		break;
+	case 2:
 		BubbleSort(A);
-	case '3':
-		MergeSort(A);
-	case '4':
-		QuickSort(A);
+		break;
+	case 3:
+		MergeSort(A, 0, LENGTH - 1);
+		break;
+	case 4:
+		QuickSort(A, 0, LENGTH - 1);
+		break;
 	default:
 		cout << "wrong Num" << endl;
 		return 1;
